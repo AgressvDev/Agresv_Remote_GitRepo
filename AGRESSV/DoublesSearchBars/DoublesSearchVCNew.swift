9ba4ee8e442d607ce41c444e1eb8df426f82d015
Ryan Max OMelia <ryanmaxomelia@gmail.com>
2023-09-25 16:35:40 -0700

    Added code to create user view controller to not allow duplicate usernames

--- AGRESSV/DoublesSearchBars/DoublesSearchVCNew.swift
+++ AGRESSV/DoublesSearchBars/DoublesSearchVCNew.swift
@@ -7,36 +7,68 @@
 
 import UIKit
 
-class DoublesSearchVCNew: UIViewController, UISearchBarDelegate {
+class DoublesSearchVCNew: UIViewController {
+    
+    
+    
+    
+    
+    
 
     
+    @IBOutlet weak var lbl_PickPartner: UILabel!
+    
+ 
     
     
-    @IBOutlet weak var sch_SearchPartner: UISearchBar!
-    
-    
-    
-    
+    var dataSourceArrayPartner = ["player1","player2","player3","player4"]
+//    var dataSourceArrayOpponentOne = ["player1","player2","player3","player4"]
+//    var dataSourceArrayOpponentTwo = ["player1","player2","player3","player4"]
+//
+//    var filteredDataSourceArrayPartner = [String]()
+//    var filteredDataSourceArrayOpponentOne = [String]()
+//    var filteredDataSourceArrayOpponentTwo = [String]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
         
-        
+//        Table_Partner.delegate = self
+//        Table_Partner.dataSource = self
+//
+//        Table_OpponentOne.delegate = self
+//        Table_OpponentOne.dataSource = self
+//
+//        Table_OpponentTwo.delegate = self
+//        Table_OpponentTwo.dataSource = self
         
         
         
     } //end of loading
     
-
     
     
-    
-    
-    
-    
-    
-
 } //end of class
+    
+    
+//extension DoublesSearchVCNew: UITableViewDelegate, UITableViewDataSource {
+//
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//
+//            return dataSourceArrayPartner.count
+//
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//            cell.textLabel?.text = dataSourceArrayPartner[indexPath.row]
+//            return cell
+//
+//
+//    }
+//}
+    
+    
+
--- AGRESSV/Views/CreateAccountViewController.swift
+++ AGRESSV/Views/CreateAccountViewController.swift
@@ -33,8 +33,12 @@ class CreateAccountViewController: UIViewController {
        
     }
     
+    //function to check if username already exists
     
-    
+  
+   
+
+
     
     @IBAction func CreateMyAccountClicked(_ sender: UIButton) {
         guard let Email = CreateEmailTextField.text else {return}
@@ -73,10 +77,64 @@ class CreateAccountViewController: UIViewController {
             }
             
             else {
-                //add user to Agressv_Users collection
-                self.addUser(Username: Username, Email: Email)
-                //go to User's Homescreen
-                self.performSegue(withIdentifier: "CreateAccountGoToHome", sender: self)
+                
+                //add code to prevent duplicate Usernames
+                //if exists query wherefield "Username" isequalto: Username then raise dialogMessage
+                func checkUsernameExists(usernameinner: String, completion: @escaping (Bool, Error?) -> Void) {
+                    let db = Firestore.firestore()
+                    let usernameref = db.collection("Agressv_Users")
+                    
+                    // Query the "users" collection to check if the username exists
+                    usernameref.whereField("Username", isEqualTo: usernameinner).getDocuments { (querySnapshot, error) in
+                        if let error = error {
+                            // Handle any errors
+                            completion(false, error)
+                        } else {
+                            // Check if there are any documents in the query result
+                            if let documents = querySnapshot?.documents, !documents.isEmpty {
+                                // Username exists
+                                completion(true, nil)
+                            } else {
+                                // Username doesn't exist
+                                completion(false, nil)
+                            }
+                        }
+                    }
+                }
+                
+                checkUsernameExists(usernameinner: Username) { (usernameExists, error) in
+                    if let error = error {
+                        print("Error checking username existence: \(error.localizedDescription)")
+                    } else {
+                        if usernameExists {
+                            
+                            let dialogMessageUsername = UIAlertController(title: "Error.", message: "That Username already exists.", preferredStyle: .alert)
+
+                            // Create OK button with action handler
+                            let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+                                print("Ok button tapped")
+                             })
+
+                            //Add OK button to a dialog message
+                            dialogMessageUsername.addAction(ok)
+                            // Present Alert to
+                            self.present(dialogMessageUsername, animated: true, completion: nil)
+                            
+                            
+                            
+                        } else {
+                            
+                            //add user to Agressv_Users collection
+                            self.addUser(Username: Username, Email: Email)
+                            //go to User's Homescreen
+                            self.performSegue(withIdentifier: "CreateAccountGoToHome", sender: self)
+                           
+                        }
+                    }
+                }
+                
+                
+                
             }
         }
     }
