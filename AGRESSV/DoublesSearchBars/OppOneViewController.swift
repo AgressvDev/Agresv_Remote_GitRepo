b092af9f8baffdb1b5edb19b5219d0636858f193
Ryan Max OMelia <ryanmaxomelia@gmail.com>
2023-09-27 01:16:31 -0700

    search bar started on opponent one screen

--- AGRESSV/DoublesSearchBars/OppOneViewController.swift
+++ AGRESSV/DoublesSearchBars/OppOneViewController.swift
@@ -11,12 +11,37 @@ class OppOneViewController: UIViewController {
 
     
     
+    @IBOutlet weak var lbl_OpponentOne: UILabel!
+    
+
+
+    @IBOutlet weak var SB_OpponentOne: UISearchBar!
+    
+    
+    @IBOutlet weak var Table_OpponentOne: UITableView!
+    
+    var dataSourceArrayOpponentOne = [String]()
+    var filteredDataSourceArrayOpponentOne = [String]()
+    
+    
+    var searching = false
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let gradientLayer = CAGradientLayer()
         
+        gradientLayer.frame = view.bounds
+        
+        gradientLayer.colors = [UIColor.black.cgColor, UIColor.white.cgColor] //UIColor.red.cgColor]
+        
+        gradientLayer.shouldRasterize = true
+        
+        //GradientPartnerbackground.layer.addSublayer(gradientLayer)
+        
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
         
         
        
