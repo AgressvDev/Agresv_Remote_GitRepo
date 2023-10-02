3d45b4657b77374ed43777bd27094f7d3c4a68e0
Ryan Max OMelia <ryanmaxomelia@gmail.com>
2023-10-02 01:21:16 -0700

    Forgot Password function added

--- AGRESSV/Views/LoginViewController.swift
+++ AGRESSV/Views/LoginViewController.swift
@@ -13,6 +13,7 @@ class LoginViewController: UIViewController {
 
     @IBOutlet var backgroundGradView: UIView!
   
+    
     @IBOutlet weak var EmailTextField: UITextField!
     @IBOutlet weak var PasswordTextField: UITextField!
     
@@ -33,10 +34,65 @@ class LoginViewController: UIViewController {
         
         self.view.layer.insertSublayer(gradientLayer, at: 0)
         
+        
+        
     }
     
+    @IBAction func btn_ForgotPassword(_ sender: UIButton) {
+        
+        
+        // Create an alert controller
+        let alertController = UIAlertController(title: "Forgot Password", message: "Enter your email address to reset your password", preferredStyle: .alert)
+        
+        // Add a text field for the user to enter their email
+        alertController.addTextField { textField in
+            textField.placeholder = "Email"
+            textField.keyboardType = .emailAddress
+        }
+        
+        // Add a "Reset Password" action
+        let resetPasswordAction = UIAlertAction(title: "Reset Password", style: .default) { _ in
+            if let email = alertController.textFields?.first?.text {
+                // Check if the email field is not empty
+                if !email.isEmpty {
+                    Auth.auth().sendPasswordReset(withEmail: email) { error in
+                        if let error = error {
+                            // Handle the error, such as displaying an alert to the user
+                            print("Password reset error: \(error.localizedDescription)")
+                        } else {
+                            // Password reset email sent successfully
+                            print("Password reset email sent")
+                        }
+                    }
+                } else {
+                    // Email field is empty, show an error message
+                    print("Email field is empty")
+                }
+            }
+        }
     
+        
     
+                
+                // Add a "Cancel" action
+                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+                
+                // Add the actions to the alert controller
+        alertController.addAction(resetPasswordAction)
+        alertController.addAction(cancelAction)
+                
+                // Present the alert controller
+                self.present(alertController, animated: true, completion: nil)
+            }
+        
+
+
+    
+
+
+
+
+
     @IBAction func LoginClicked(_ sender: UIButton) {
         guard let email = EmailTextField.text else {return}
         guard let password = PasswordTextField.text else {return}
@@ -64,4 +120,5 @@ class LoginViewController: UIViewController {
     }
     
     
+
 }
