b6a1919371a8ba0268c7a03154d7212d861368b6
Ryan Max OMelia <ryanmaxomelia@gmail.com>
2024-10-10 17:32:15 -0700

    test push

--- AGRESSV/Groups/GroupsHeaderViewController.swift
+++ AGRESSV/Groups/GroupsHeaderViewController.swift
@@ -18,7 +18,21 @@ class GroupsHeaderViewController: UIViewController {
 
         
         
-        
+        // Create the label
+               let lbl_Groups = UILabel()
+               lbl_Groups.text = "Groups"
+               lbl_Groups.textColor = .white
+               lbl_Groups.font = UIFont.boldSystemFont(ofSize: 25) // Set the desired font size
+               lbl_Groups.translatesAutoresizingMaskIntoConstraints = false
+               
+               // Add the label to the view
+               view.addSubview(lbl_Groups)
+               
+               // Set constraints to position the label at the top left
+               NSLayoutConstraint.activate([
+                   lbl_Groups.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+                   lbl_Groups.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 25)
+               ])
         
         // Set up the background image
         setupBackgroundImage()
